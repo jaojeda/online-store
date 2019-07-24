8b59bb47f6a00bdab1dc5e9df7ef3368c57fafa5
jaojeda <j.a.ojeda@live.com>
2019-07-24 11:36:29 -0700

    created JS for shopping cart, updated shoppingcart html with functions to render line and to calcluate order total

---
+++ src/shopping-cart.js
@@ -0,0 +1,22 @@
+import order from './data/order.js';
+import products from './data/f&s-data.js';
+import { renderLineItem } from './render-line-item.js';
+import { findProduct, getOrderTotal } from './register.js';
+import { toUSD } from  './currency.js';
+
+const tbody = document.querySelector('tbody');
+const totalDisplay = document.getElementById('order-total');
+
+for(let i = 0; i < order.length; i++) {
+    const itemLine = order[i];
+    const product = findProduct(products, itemLine.code);
+    const dom = renderLineItem(itemLine, product);
+
+    tbody.appendChild(dom);
+}
+
+// calc order total
+const orderTotal = getOrderTotal(order, products);
+
+// assign to total cell
+totalDisplay.textContent = toUSD(orderTotal);
