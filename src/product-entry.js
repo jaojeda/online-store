d051ffe1956371aa4d0c6aa30c04187da6a38124
jaojeda <j.a.ojeda@live.com>
2019-07-26 10:45:59 -0700

    crated product-entry.js, added script to html file, tested product was added!

--- src/product-entry.js
+++ src/product-entry.js
@@ -2,3 +2,36 @@ import store from './data/store.js';
 
 const form = document.getElementById('new-product-form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const image = formData.get('image');
+    getBase64(image, (base64Url) => {
+        
+        const product = {
+            code: formData.get('code'),
+            name: formData.get('name'),
+            image: base64Url,
+            description: formData.get('description'),
+            category: formData.get('category'),
+            price: +formData.get('price'),
+            cost: +formData.get('cost'),
+        };
+    
+        store.addProduct(product);
+    
+        alert('Product saved!');
+    
+        form.reset();
+    });
+});
+
+function getBase64(file, callback) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function() {
+        callback(reader.result);
+    };
+}
